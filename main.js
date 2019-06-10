33e63674e950114fc4ec94d82081d08708d88120
Kyle Knutson <jissensmasher@gmail.com>
2019-06-10 12:50:36 -0700

    Add html, css, and js files

---
+++ main.js
@@ -0,0 +1,116 @@
+    "use strict";
+
+const url = "https://kitsu.io/api/edge/anime"; //data.attributes.posterImage
+const APIKey = 'AIzaSyARlWxggTnR6mIMhHdyW2Bnf7BomchaBaE';
+
+
+function handle_SynopsisButton() {
+	$('input.synopsis').on("click", function(e) { //need to select only
+		e.stopImmediatePropagation();
+		const cardEl = $(e.currentTarget).parents('div.card').first();
+		const trailerEl = $(e.currentTarget).parents('div.info').next();
+		if (!trailerEl.hasClass('hidden')) {
+			trailerEl.addClass('hidden');
+			cardEl.toggleClass('expand');
+		};
+		cardEl.toggleClass('expand');
+		$(e.currentTarget).parents('div.info').first().nextAll('div.synopsis').toggleClass('hidden');
+	});
+}
+
+function handle_TrailerButton() {
+	$('input.trailer').on("click", function(e) { //need to select only
+		e.stopImmediatePropagation();
+		const cardEl = $(e.currentTarget).parents('div.card').first();
+		const synopsisEl = $(e.currentTarget).parents('div.info').first().nextAll('div.synopsis');
+		if (!synopsisEl.hasClass('hidden')) {
+			synopsisEl.toggleClass('hidden');
+			cardEl.toggleClass('expand');
+		};
+		cardEl.toggleClass('expand');
+		$(e.currentTarget).parent().parent('div.info').next().toggleClass('hidden'); 
+	});
+}
+
+function addCardButtons() {
+	handle_SynopsisButton();
+	handle_TrailerButton();
+}
+
+function injectTemplate(element) {
+	const cardTemplate = 
+		`<div class="card border">
+			<img src="${element.attributes.posterImage.tiny}">
+			<div class="info">
+				<h3 class="title">${element.attributes['titles']['en']}</h3>
+
+				<p><i class="fas fa-heart" style="color: #ff3575"></i>  ${element.attributes.averageRating}% Approval</p>
+				<p>Rated <strong>${element.attributes.ageRating}</strong></p>
+				<p>${element.attributes.episodeCount} episodes</p>
+
+				<div class="button-container">
+					<input class="synopsis btn" type="button" name="synopsis-btn" value="Synopsis">
+					<input class="trailer btn" type="button" name="trailer-btn" value="Trailer">
+				</div>
+			</div>
+			
+			<div class="trailer hidden">
+				<h4 class="trailer">Trailer</h4>
+				<iframe width="320" height="240" src="https://www.youtube.com/embed/${element.attributes.youtubeVideoId}"></iframe>
+			</div>
+
+			<div class="synopsis hidden">
+				<h4 class="synopsis">Synopsis</h4>
+				<p>${element.attributes.synopsis}</p>
+			</div>
+		</div>`;
+	return cardTemplate;
+}
+
+function formatSearchResults(responseJson) {
+	$('#results').empty();
+	$('#results').append( `<h2>Results</h2>` );
+	responseJson['data'].forEach( element => {
+		$('#results').append( 
+			injectTemplate(element)
+		);
+		addCardButtons();
+	})
+}
+
+function formatQueryParams(params) {
+	const queryItems = Object.keys(params)
+	.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+	return queryItems.join('&');
+}
+
+function searchAnime(search) {
+	const params = {
+		'filter[text]': search,
+		'page[limit]': 10,
+		'page[offset]': 0 
+	}
+	const queryString = formatQueryParams(params);
+	const searchURL = url + '?' + queryString;
+
+	fetch(searchURL)
+	.then(response => response.json())
+	.then(responseJson => {
+		formatSearchResults(responseJson);
+	})
+}
+
+function watchForm() {
+
+	$('body').toggleClass('fade');
+	searchAnime("death"); // for debugging purpposes...
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		const searchTitle = $('.search-title').val();
+		searchAnime(searchTitle);
+	});
+}
+
+$(watchForm);
+
